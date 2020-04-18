146d1f96b374f49990fccff7db3199130684e063
Bhart sharma <59437165+BhartSharmawhitehathacker@users.noreply.github.com>
2020-04-18 20:37:23 +0530

    Add files via upload

---
+++ bsfrecon.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+red='\033[1;31m'
+blue='\033[1;34m'
+white='\033[1;37m'
+green='\033[1;32m'
+
+printf "$green"
+
+figlet -f big bsf finder . . .  
+
+echo "<><><><>><><<><><><><><>><><><><
+
+"
+
+echo "author : Bhart Sharma"
+echo ">>>>>>>>>>>>>>>>>>>>>"
+echo "Instagram : varun_sharma4578" 
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+echo "website : bsf-installer.official.blogspot.com"
+
+echo "================================
+
+"
+echo "set UP Done ....
+
+" 
+
+echo "/////////////////////////////////////////"
+echo "input Username"
+read -p ">|" T1
+clear
+printf "$green"
+figlet -f big FINDER
+
+printf "$white"
+
+echo "These Are Your Target Profile Link In All Social Media
+
+"
+
+echo " [ Info ] Checking On Instagram 
+
+" 
+
+printf "$white"
+echo "Found
+
+" 
+
+
+printf "$green"
+echo " https://www.instagram.com/$T1 
+
+" 
+
+printf "$white"
+
+echo " [ Info ] Checking  On Facebook 
+
+" 
+
+printf "$white"
+echo "Found 
+
+" 
+
+printf "$green"
+echo "https://www.facebook.com/$T1
+
+" 
+
+
+printf "$white"
+echo "[ Info ] Checking on Youtube 
+
+" 
+
+printf "$white"
+echo "Found 
+
+" 
+
+printf "$green"
+
+echo "https://www.youtube.com/$T1
+
+" 
+
+printf "$white"
+echo "[ Info ] checking On Twitter ...
+
+" 
+
+printf "$white"
+echo "Found  
+
+" 
+
+
+printf "$green"
+echo "https://www.twitter.com/$T1
+
+"
+
+printf "$white"
+echo "[ Info ] checking On googleplus
+
+" 
+
+echo "Found
+
+" 
+
+printf "$green"
+
+echo "https://www.googleplus.com/$T1
+
+" 
+
+echo "Trying Number
+
+" 
+
+
+echo "get number From Facebook $T1
+
+" 
+
+echo " Trying  number found 
+
+" 
+
+echo "check on Facebook 
+
+" 
+
+printf "$white"
+echo "[ Info ] checking On Github
+
+" 
+
+echo "Found 
+
+" 
+
+printf "$green"
+echo "https://github.com/$T1
+
+" 
+
+
+printf "$white"
+echo "[Info] checking On Spotify 
+
+"
+
+printf "$white"
+
+echo "Found
+
+" 
+
+printf "$green"
+
+echo "https://spotify.com/$T1
+
+" 
